0e422649e76430224d52fe943226c04598fd6dbe
adam walocha <contact@codedquality.com>
2016-02-09 14:05:38 +0100

    Basic structure

---
+++ WpfCali/WpfCali/MainBoostrapper.cs
@@ -0,0 +1,46 @@
+﻿using Caliburn.Micro;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfCali.ViewModels;
+
+namespace WpfCali
+{
+    public class MainBootstrapper : BootstrapperBase
+    {
+        private UnityContainer container;
+
+        public MainBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            container = new UnityContainer();
+            container.RegisterInstance<IWindowManager>(new WindowManager());
+            container.RegisterInstance<IEventAggregator>(new EventAggregator());
+
+        }
+
+        protected override object GetInstance(Type serviceType, string key)
+        {
+            return container.Resolve(serviceType, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.ResolveAll(service);
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ WpfCali/WpfCali/ViewModels/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfCali.ViewModels
+{
+    public class MainViewModel : Conductor<object>
+    {
+        IWindowManager _windowManager;
+        Func<InvoicesListingViewModel> _invoicesListingViewModelLocator;
+        Func<OrdersListingViewModel> _ordersListingViewModelLocator;
+        public MainViewModel(IEventAggregator eventAggregator, IWindowManager windowManager, Func<InvoicesListingViewModel> invoicesListingViewModelLocator, Func<OrdersListingViewModel> ordersListingViewModelLocator)
+        {
+            _windowManager = windowManager;
+            _invoicesListingViewModelLocator = invoicesListingViewModelLocator;
+            _ordersListingViewModelLocator = ordersListingViewModelLocator;
+        }
+
+
+        public bool CanShowInvoices
+        {
+            get { return true; }
+        }
+
+        public void ShowInvoices()
+        {
+            ActivateItem(_invoicesListingViewModelLocator());
+
+        }
+
+        public void ShowOrders()
+        {
+            ActivateItem(_ordersListingViewModelLocator());
+
+        }
+    }
+}
